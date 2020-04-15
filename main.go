14f52da2788fbce47f0d985ac9b33070bdb36fa5
Charlie Jonas <charlie@charliejonas.co.uk>
2020-04-15 23:31:21 +0100

    Add basic ping page

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
 
 	"github.com/cbroglie/mustache"
 )
@@ -12,7 +13,16 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		context := map[string]string{"title": "Home Page", "name": "World"}
 		str, _ := mustache.RenderFileInLayout("assets/home.html.mustache", "assets/layout.html.mustache", context)
-		fmt.Fprintf(w, str)
+		fmt.Fprint(w, str)
 	})
+
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		cmd := exec.Command("ping", "-c", "4", "1.1.1.1")
+		stdout, _ := cmd.Output()
+		context := map[string]string{"title": "Ping Cloudflare", "code": string(stdout)}
+		str, _ := mustache.RenderFileInLayout("assets/ping.html.mustache", "assets/layout.html.mustache", context)
+		fmt.Fprint(w, str)
+	})
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
