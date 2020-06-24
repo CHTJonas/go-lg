17a06157916d6a07752baf7e13e15f1d0dae3e11
Charlie Jonas <charlie@charliejonas.co.uk>
2020-06-24 23:41:39 +0100

    Add data persistence WIP

--- main.go
+++ main.go
@@ -1,27 +1,48 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
 	"os/exec"
+	"strings"
 
 	"github.com/cbroglie/mustache"
+	"github.com/dgraph-io/badger"
 )
 
+var db *badger.DB
+
 func main() {
+	bdb, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
+	if err != nil {
+		panic(err)
+	}
+	db = bdb
+	defer bdb.Close()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		context := map[string]string{"title": "Home Page", "name": "World"}
 		str, _ := mustache.RenderFileInLayout("assets/home.html.mustache", "assets/layout.html.mustache", context)
 		fmt.Fprint(w, str)
 	})
 
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		cmd := exec.Command("ping", "-c", "4", "1.1.1.1")
-		stdout, _ := cmd.Output()
-		context := map[string]string{"title": "Ping Cloudflare", "code": string(stdout)}
-		str, _ := mustache.RenderFileInLayout("assets/ping.html.mustache", "assets/layout.html.mustache", context)
-		fmt.Fprint(w, str)
+	http.HandleFunc("/ping/", func(w http.ResponseWriter, r *http.Request) {
+		var stdout []byte
+		uid := strings.TrimPrefix(r.URL.Path, "/ping/")
+		if uid != "" {
+			stdout = Read(uid)
+			context := map[string]string{"title": "Ping Cloudflare", "code": string(stdout)}
+			str, _ := mustache.RenderFileInLayout("assets/ping.html.mustache", "assets/layout.html.mustache", context)
+			fmt.Fprint(w, str)
+		} else {
+			cmd := exec.Command("ping", "-c", "4", "1.1.1.1")
+			stdout, _ := cmd.Output()
+			key, _ := Write(stdout)
+			http.Redirect(w, r, r.URL.String()+"/"+key, http.StatusTemporaryRedirect)
+		}
 	})
 
 	http.HandleFunc("/traceroute", func(w http.ResponseWriter, r *http.Request) {
@@ -33,4 +54,32 @@ func main() {
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func Read(key string) []byte {
+	var stdout []byte
+	db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(key))
+		if err != nil {
+			return err
+		}
+		return item.Value(func(val []byte) error {
+			stdout = append([]byte{}, val...)
+			return nil
+		})
+	})
+	return stdout
+}
+
+func Write(stdout []byte) (string, error) {
+	uid := GenerateUID()
+	return uid, db.Update(func(txn *badger.Txn) error {
+		return txn.Set([]byte(uid), stdout)
+	})
+}
+
+func GenerateUID() string {
+	token := make([]byte, 6)
+	rand.Read(token)
+	return hex.EncodeToString(token)
 }
